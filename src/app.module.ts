f5472f7b056ba8c8c1074804a2f98e997ab9b81d
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2024-09-25 23:49:47 +0100

    feat: code quality set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RecipeModule } from './recipe/recipe.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, RecipeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
